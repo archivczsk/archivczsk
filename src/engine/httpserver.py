428dbcfc4fc79431808bf37e0a2f442f2176e728
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2023-01-24 10:05:12 +0100

    auto convert strings to bytes in http handler

--- src/engine/httpserver.py
+++ src/engine/httpserver.py
@@ -4,6 +4,7 @@ from twisted.web import server, http, resource
 from twisted.internet import defer
 from Plugins.Extensions.archivCZSK import _, log
 from Components.config import config
+from ..py3compat import *
 
 class AddonHttpRequestHandler(resource.Resource):
 	isLeaf = True
@@ -21,34 +22,51 @@ class AddonHttpRequestHandler(resource.Resource):
 		self.NOT_DONE_YET = server.NOT_DONE_YET
 		self.name = AddonHttpRequestHandler.addonIdToEndpoint(addon_id)
 		self.prefix_len = len(self.name)+2
-	
+
+	def __to_bytes(self, data):
+		if isinstance(data, unicode):
+			return data.encode('utf-8')
+
+		return data
+
+	def get_endpoint(self, request, relative=False):
+		if relative:
+			return "/%s" % self.name
+		else:
+			server_name = request.getRequestHostname()
+			server_port = request.getHost().port
+			return "http://%s:%d/%s" % (server_name, server_port, self.name)
+
 	def reply_error404(self, request):
 		request.setHeader("content-type", "text/html")
 		request.setResponseCode(http.NOT_FOUND)
 		data = "<html><head><title>archivCZSK</title></head><body><h1>Error 404: addon %s has not set any response</h1><br />The requested URL was not found on this server.</body></html>" % self.name
-		return data.encode('utf-8')
+		return self.__to_bytes(data)
 
 	def reply_error500(self, request):
 		request.setHeader("content-type", "text/html")
 		request.setResponseCode(http.INTERNAL_SERVER_ERROR)
 		data = "<html><head><title>archivCZSK</title></head><body><h1>Error 500: addon %s failed</h1><br />Internal server error</body></html>" % self.name
-		return data.encode('utf-8')
+		return self.__to_bytes(data)
 
 	def reply_redirect(self, request, redirect_url ):
-		request.redirect( redirect_url )
+		request.redirect( self.__to_bytes(redirect_url) )
 		request.setHeader("content-type", "text/plain")
 		request.finish()
 		return server.NOT_DONE_YET
 
-	def reply_ok(self, request, data, content_type=None ):
+	def reply_ok(self, request, data, content_type=None, raw=False ):
 		if content_type:
 			request.setHeader("content-type", content_type )
 			
 		request.setResponseCode(http.OK)
-		return data
+		if raw:
+			return data
+		else:
+			return self.__to_bytes(data)
 	
 	def get_relative_path(self, request ):
-		return request.path[self.prefix_len:]
+		return request.path.decode('utf-8')[self.prefix_len:]
 		
 	def render(self, request):
 		# if addon wants to handle requests more flexible, then it can override this function
@@ -59,21 +77,21 @@ class AddonHttpRequestHandler(resource.Resource):
 		# https://twistedmatrix.com/documents/21.2.0/api/twisted.web.http.Request.html
 		
 		path_full = self.get_relative_path( request )
-		path = path_full.split(b'/')[0]
+		path = path_full.split('/')[0]
 		if len(path) > 0 and request.method == b'GET':
-			func = getattr(self, "P_" + path.decode('utf-8'), None)
+			func = getattr(self, "P_" + path, None)
 			
 			if callable(func):
-				return func(request, path_full[len(path)+1:])
+				return self.__to_bytes((func(request, path_full[len(path)+1:])))
 		
-		return self.default_handler( request, path_full )
+		return self.__to_bytes(self.default_handler( request, path_full ))
 	
 	def default_handler(self, request, path_full ):
 		# this is default handler, when request is not processed by named endpoint - it mostly prints error message 
 		request.setHeader("content-type", "text/plain; charset=utf-8")
 		request.setResponseCode(http.NOT_FOUND)
 		data = "Error 404: addon %s has no handler for path %s" % (self.name, path_full)
-		return data.encode('utf-8')
+		return self.__to_bytes(data)
 
 
 class ArchivCZSKHttpServer:
