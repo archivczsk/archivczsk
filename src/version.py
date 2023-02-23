4089923756a09d17f79853faeb47ab2cdb0db43c
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2023-02-23 16:14:29 +0100

    release version 2.0.2

--- src/version.py
+++ src/version.py
@@ -1,7 +1,7 @@
 from . import _
 
 title = _("ArchivCZSK")
-version = "2.0.1"
+version = "2.0.2"
 author = "archivczsk"
 description = _("Playing CZ/SK archives")
 url = "https://github.com/archivczsk/archivczsk/"
