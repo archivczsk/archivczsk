99b1a427c39a9cf7b006f06b007edb45c21a4a81
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2023-03-03 11:00:11 +0100

    update lang script

--- build/rebuild_lang.sh
+++ build/rebuild_lang.sh
@@ -8,16 +8,10 @@ PY_FILES=$(find src -name '*.py' | grep -v src/resources/libraries)
 for lang in cs sk ; do
 	# extract clean list of strings
 	xgettext -L python $PY_FILES --no-wrap --foreign-user --package-name=enigma2-plugin-extension-archivczsk --package-version='' --copyright-holder='' -o ${lang}.pot
-
-	# mark obsolete strings
-	msgattrib --set-obsolete --ignore-file=${lang}.pot -o locale/${lang}.po locale/${lang}.po
 	
-	# remove obsolete strings
-	msgattrib --no-obsolete -o locale/${lang}.po locale/${lang}.po
+	# merge old translated strings to clean list
+	msgmerge -U --no-wrap -N --backup=none --lang=${lang} locale/${lang}.po ${lang}.pot
 	
 	# remove clean strings file
 	rm ${lang}.pot
-	
-	# add new strings
-	xgettext -L python $PY_FILES --no-wrap --foreign-user --package-name=enigma2-plugin-extension-archivczsk --package-version='' --copyright-holder='' -j -o locale/${lang}.po
 done
