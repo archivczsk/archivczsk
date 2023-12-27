716d261aee2dea08f0777433521e9698764a3d62
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2023-12-27 12:13:22 +0100

    added global error handling for requests library

--- src/gui/exception.py
+++ src/gui/exception.py
@@ -15,6 +15,7 @@ from .common import showInfoMessage, showWarningMessage, showErrorMessage
 from ..engine.exceptions import addon, download, play
 from ..gsession import GlobalSession
 from .. import _, log
+import requests
 
 class GUIExceptionHandler(object):
 	errorName = _("Unknown Error")
@@ -66,17 +67,33 @@ class AddonExceptionHandler(GUIExceptionHandler):
 					log.logError("Addon (AddonError) error '%s'.\n%s"%(er.value,traceback.format_exc()))
 					self.errorMessage(er.value)
 				# loading exceptions
-				except HTTPError as e:
+				except (requests.HTTPError, HTTPError) as e:
 					log.logError("Addon (HTTPError) error '%s'.\n%s"%(e.code,traceback.format_exc()))
-					message = "%s %s:%d" % (_("Error in loading"), _("HTTP Error"), e.code)
+					message = "%s: %s %d" % (_("Error in loading"), _("HTTP Error"), e.code)
 					self.errorMessage(message)
 				except URLError as e:
 					log.logError("Addon (URLError) error '%s'.\n%s"%(e.reason,traceback.format_exc()))
-					message = "%s %s:%s" % (_("Error in loading"), _("URL Error"), str(e.reason))
+					message = "%s: %s\n%s" % (_("Error in loading"), _("URL Error"), str(e.reason))
 					self.errorMessage(message)
 				except addon.AddonThreadException as er:
 					log.logError("Addon (AddonThreadException) error.\n%s"%(traceback.format_exc()))
 					pass
+				except requests.ConnectTimeout as e:
+					log.logError("Addon (ConnectTimeout) error '%s'.\n%s"%(str(e),traceback.format_exc()))
+					message = "%s: %s" % (_("Error in loading"), _("Connection to server timed out"))
+					self.errorMessage(message)
+				except requests.ReadTimeout as e:
+					log.logError("Addon (ReadTimeout) error '%s'.\n%s"%(str(e),traceback.format_exc()))
+					message = "%s: %s" % (_("Error in loading"), _("Waiting for data from server timed out"))
+					self.errorMessage(message)
+				except requests.ConnectionError as e:
+					log.logError("Addon (ConnectionError) error '%s'.\n%s"%(str(e),traceback.format_exc()))
+					message = "%s: %s" % (_("Error in loading"), _("Connection to server failed"))
+					self.errorMessage(message)
+				except requests.RequestException as e:
+					log.logError("Addon (RequestException) error '%s'.\n%s"%(str(e),traceback.format_exc()))
+					message = "%s: %s\n%s" % (_("Error in loading"), _("Connection to server failed"), str(e))
+					self.errorMessage(message)
 				# we handle all possible exceptions since we dont want plugin to crash because of addon error...
 				except Exception as e:
 					log.logError("Addon error.\n%s"%traceback.format_exc())
